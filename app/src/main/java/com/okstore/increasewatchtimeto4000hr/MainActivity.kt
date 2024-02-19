cdc6bb4c6157793b9ef94002abbad2f0acdad290
Arjun Gupta <arjungupta0817@gmail.com>
2024-02-19 15:35:22 +0530

    splash screen

--- app/src/main/java/com/okstore/increasewatchtimeto4000hr/MainActivity.kt
+++ app/src/main/java/com/okstore/increasewatchtimeto4000hr/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.okstore.increasewatchtimeto4000hr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import com.okstore.increasewatchtimeto4000hr.dashboard.DashboardActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val handler = Handler(Looper.getMainLooper())
+
+        handler.postDelayed(Runnable {
+             startActivity(Intent(this, DashboardActivity::class.java))
+        }, 200 )
+
     }
 }
